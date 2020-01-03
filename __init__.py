f37e027e874d0adbc71134650801cb16370a409a
paulfogel <paul_fogel@hotmail.com>
2020-01-03 15:25:55 +0100

    Remove backup copy of modules

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 """
 Non-negative matrix and tensor factorization init file
 
-Imports all modules in subfolder modules
+Imports subfolder modules
 
 """
 
 # Author: Paul Fogel
 
 # License: MIT
-# Dec 28, '19
+# Jan 3 '20
 
 import os, sys, inspect
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],'modules')))
