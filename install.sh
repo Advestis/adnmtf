fa89e5b0a80610efdef8d50ec71d2627d5650ff9
pcotte <pcotte@advestis.com>
2020-09-03 18:44:44 +0200

    chg: dev: added check in install when deleting directories

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pip3 uninstall nmtf -y || echo "No nmtf to uninstall"
 pip3 install setuptools
 python3 setup.py install
-rm -r dist
-rm -r build
-rm -r nmtf.egg-info*
+if [ -d "dist" ] ; then rm -r dist ; fi
+if [ -d "build" ] ; then rm -r build ; fi
+if ls nmtf.egg-info* &> /dev/null ; then rm -r nmtf.egg-info* ; fi
