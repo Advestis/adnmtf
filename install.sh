0f5f33e79c60a6ae8eb5bcefa0fee935c1fb0006
pcotte <pcotte@advestis.com>
2020-09-04 19:19:12 +0200

    chg: dev: Uses gspip in both install and actions

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
-pip3 uninstall nmtf -y || echo "No nmtf to uninstall"
+#!/bin/bash
+publish=false
+
+PACKAGE="nmtf"
+
+while true; do
+  case "$1" in
+    -p | --publish) publish=true ; shift 1 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+pip3 uninstall "$PACKAGE" -y || echo "No $PACKAGE to uninstall"
 pip3 install setuptools
 python3 setup.py install
+if $publish && [ -f "$HOME/bin/gspip" ] ; then
+  gspip push -s "  "
+fi
 if [ -d "dist" ] ; then rm -r dist ; fi
 if [ -d "build" ] ; then rm -r build ; fi
-if ls nmtf.egg-info* &> /dev/null ; then rm -r nmtf.egg-info* ; fi
+if ls "$PACKAGE".egg-info* &> /dev/null ; then rm -r "$PACKAGE".egg-info* ; fi
