cd4c2028897673862b3a70265cf7297e1e1d141f
pcotte <pcotte@advestis.com>
2020-09-11 16:38:34 +0200

    chg: usr: removed rmdir line in install.bat. Fixed float comparison in pytest and changed expected results

--- install.bat
+++ install.bat
@@ -1,4 +1,2 @@
 python setup.py bdist_wininst
-IF exist dist\ ( rmdir dist )
-IF exist build\ ( rmdir build )
 pdoc --force --html --config show_source_code=False --output-dir docs nmtf
--- tests/unit/test_ntf.py
+++ tests/unit/test_ntf.py
@@ -1,12 +1,38 @@
 from nmtf import NTF
 
+import pytest
 import pandas as pd
 import numpy as np
 import json
 import sys
 from pathlib import Path
+from datetime import date, datetime
 
 DATA_PATH = Path(__file__).parent.parent / "data"
+
+
+class JSONEncoder(json.JSONEncoder):
+    """
+    Extending the JSON encoder so it knows how to serialise a dataframe
+    """
+
+    def default(self, obj):
+        if hasattr(obj, "to_json"):
+            if callable(obj.to_json):
+                try:
+                    return obj.to_json(orient="records")
+                except TypeError:
+                    return obj.to_json()
+            else:
+                return obj.to_json
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, date):
+            return obj.strftime("%Y-%m-%d")
+        elif isinstance(obj, datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 
 def test():
@@ -15,12 +41,19 @@ def test():
     with open(DATA_PATH / "expected_result_ntf.json", "r") as ifile:
         decoded_array = json.load(ifile)
         for key in decoded_array:
-            expected_estimator[key] = np.asarray(decoded_array[key])
+            expected_estimator[key] = (
+                np.asarray(decoded_array[key]) if isinstance(decoded_array[key], list) else decoded_array[key]
+            )
     m0 = df.values
     n_blocks = 5
     my_nt_fmodel = NTF(n_components=5)
-    estimator = my_nt_fmodel.fit_transform(m0, n_blocks, sparsity=.8, n_bootstrap=10)
+    estimator = my_nt_fmodel.fit_transform(m0, n_blocks, sparsity=0.8, n_bootstrap=10)
     estimator = my_nt_fmodel.predict(estimator)
+
+    # Uncomment to save the estimator in a file
+    # with open(DATA_PATH / "expected_result_ntf_new.json", "w") as ofile:
+    #     ofile.write(json.dumps(estimator, cls=JSONEncoder))
+
     failed = False
     for key in estimator:
         print("")
@@ -43,6 +76,8 @@ def test():
         try:
             if isinstance(estimator[key], np.ndarray):
                 np.testing.assert_array_almost_equal(estimator[key], expected_estimator[key_exp])
+            elif isinstance(estimator[key], float):
+                assert pytest.approx(estimator[key], 1e-10) == pytest.approx(expected_estimator[key], 1e-10)
             else:
                 assert estimator[key] == expected_estimator[key_exp]
             print("...ok")
