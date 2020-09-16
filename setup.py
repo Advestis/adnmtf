a6123e77f693a9517d399ee55a27ada56e118626
pcotte <pcotte@advestis.com>
2020-09-16 16:40:26 +0200

    fix: dev: Corrected setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,35 @@ def run_cmd(cmd):
     return subprocess.check_output(cmd).decode(encoding="UTF-8").split("\n")
 
 
+def get_greatest_version(versions: List[str]) -> str:
+    versions = [list(map(int, v[1:].split("."))) for v in versions]
+    greatest = None
+    for v in versions:
+        if greatest is None:
+            greatest = v
+        else:
+            lower = False
+            for i in range(len(v)):
+                if len(greatest) < i + 1:
+                    greatest = v
+                    break
+                if v[i] > greatest[i]:
+                    greatest = v
+                    break
+                if v[i] < greatest[i]:
+                    lower = True
+                    break
+            if not lower:
+                greatest = v
+    return f"v{'.'.join([str(s) for s in greatest])}"
+
+
 def get_last_tag() -> str:
     result = [v for v in run_cmd("git tag -l v*") if not v == ""]
     if len(result) == 0:
         run_cmd("git tag v0.0.1")
-    result = [v for v in run_cmd("git tag -l v*") if not v == ""]
-    return result[-1]
+    result = [v for v in run_cmd("git tag -l v*") if not v == "" and v.startswith("v")]
+    return get_greatest_version(result)
 
 
 def get_nb_commits_until(tag: str) -> int:
