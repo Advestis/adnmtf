9ae175dfc745531bc00ace32c9722236c300969b
paulfogel <paul_fogel@hotmail.com>
2022-01-18 11:22:00 +0100

    minor change in test_nmf

--- tests/endtoend/test_nmf.py
+++ tests/endtoend/test_nmf.py
@@ -17,21 +17,20 @@ def test_nmf(inputs_):
         print(f"Testing {param}...")
         if param.lower() == "wb" or param.lower() == "hb":
             print(f"Ignoring {param}...")
-            return
-
-        if test_name == "test_nmf4":
-            np.savetxt(DATA_PATH / "test_nmf4_W.csv", estimator['W'])
-            np.savetxt(DATA_PATH / "test_nmf4_H.csv", estimator['H'])
-
-        param_exp = param
-        if param not in expected_estimator:
-            param_exp = param.upper()
-
-        assert param_exp in expected_estimator
-        assert isinstance(estimator[param], type(expected_estimator[param_exp]))
-        if isinstance(estimator[param], np.ndarray):
-            np.testing.assert_array_almost_equal(estimator[param], expected_estimator[param_exp])
-        elif isinstance(estimator[param], float):
-            assert pytest.approx(estimator[param], rel=1e-10) == expected_estimator[param_exp]
         else:
-            assert estimator[param] == expected_estimator[param_exp]
+            if test_name == "test_nmf4":
+                np.savetxt(DATA_PATH / "test_nmf4_W.csv", estimator['W'])
+                np.savetxt(DATA_PATH / "test_nmf4_H.csv", estimator['H'])
+
+            param_exp = param
+            if param not in expected_estimator:
+                param_exp = param.upper()
+
+            assert param_exp in expected_estimator
+            assert isinstance(estimator[param], type(expected_estimator[param_exp]))
+            if isinstance(estimator[param], np.ndarray):
+                np.testing.assert_array_almost_equal(estimator[param], expected_estimator[param_exp])
+            elif isinstance(estimator[param], float):
+                assert pytest.approx(estimator[param], rel=1e-10) == expected_estimator[param_exp]
+            else:
+                assert estimator[param] == expected_estimator[param_exp]
